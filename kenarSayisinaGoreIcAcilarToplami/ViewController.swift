73f1fcb6bb0377d139961d65d172b9b22e454746
Furkan Şengil <70530027+furkanSengil@users.noreply.github.com>
2022-02-27 10:28:44 +0300

    Initial commit

---
+++ kenarSayisinaGoreIcAcilarToplami/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  kenarSayisinaGoreIcAcilarToplami
+//
+//  Created by Furkan Sengil on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var kullaniciGirisi: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.kullaniciGirisi.delegate = self
+        
+    }
+   
+    @IBAction func hesaplamaButonu(_ sender: UIButton) {
+        var icAcilarToplami = 0
+        var sonuc = ""
+        if let kenar = Int(kullaniciGirisi.text!) {
+            icAcilarToplami = (kenar - 2) * 180
+            sonuc = String(icAcilarToplami)
+        }
+        
+        let alertController = UIAlertController(title: "Çokgenin iç açıları toplamı:", message: sonuc, preferredStyle: .alert)
+        let tamam = UIAlertAction(title: "Tamam", style: .destructive)
+        alertController.addAction(tamam)
+        self.present(alertController, animated: true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            self.view.endEditing(true)
+            return false
+        }
+
+}
+
